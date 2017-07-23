ce6e1136739fb60f11b21629a7322afe8ce234b0
Muhammad Umer <myemailum14@gmail.com>
2017-07-22 23:05:03 -0400

    Update algo.js

--- Power_digit_sum/algo.js
+++ Power_digit_sum/algo.js
@@ -10,33 +10,33 @@ For example, power_digit_sum(2, 4) would return 7:
 
 //mimic how a human would multiply as Math.pow would give number too big for JS
 function multiply(str, i) {
-	 str = String(str);
-	 let result = '';
-     let carry_over = 0;
-     for (let k = str.length-1; k >= 0; k--) {
-         let num = Number(str[k]) * i + carry_over;
-         let str_num =  String(num);
-	     if (k == 0) {
-			result = str_num.substr(0) + result;
-         } else {
-         	result = str_num.substr(-1) + result;
-			if (str_num.length > 1) {
-				carry_over = Number(str_num.slice(0,-1));
-         	} else { 
-         		carry_over = 0;
-         	}
-         }        
-     }
+    str = String(str);
+    let result = '';
+    let carry_over = 0;
+    for (let k = str.length - 1; k >= 0; k--) {
+        let num = Number(str[k]) * i + carry_over;
+        let str_num = String(num);
+        if (k == 0) {
+            result = str_num.substr(0) + result;
+        } else {
+            result = str_num.substr(-1) + result;
+            if (str_num.length > 1) {
+                carry_over = Number(str_num.slice(0, -1));
+            } else {
+                carry_over = 0;
+            }
+        }
+    }
     return result;
 }
 
 
 
-function power_digit_sum(n,p) {
-	let result = 1;
-	for(let i = 1; i < p; i++) {
-		if (i==1) result = n;
-		result = multiply(result, n);
-	}
-	return result.split('').reduce((t,x)=>Number(x)+t,0);
+function power_digit_sum(n, p) {
+    let result = 1;
+    for (let i = 1; i < p; i++) {
+        if (i == 1) result = n;
+        result = multiply(result, n);
+    }
+    return result.split('').reduce((t, x) => Number(x) + t, 0);
 }
