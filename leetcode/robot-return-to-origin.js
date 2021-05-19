dfbfb739d406c0560158789bf576321d0e1bcbdf
Muhammad Umer <myemailum14@gmail.com>
2021-05-19 00:34:05 -0400

    Create robot-return-to-origin.js

---
+++ leetcode/robot-return-to-origin.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} moves
+ * @return {boolean}
+ */
+var judgeCircle = function(moves) {
+    let x = 0, y = 0;
+    
+    for (let i = 0; i < moves.length; i++) {
+        switch (moves[i]) {
+                case 'U':
+                    y--;
+                    break;
+                case 'D':
+                    y++;
+                    break;
+                case 'L':
+                    x--;
+                    break;
+                case 'R':
+                    x++;
+                    break;
+        }
+    }
+    
+    return x === 0 && y === 0;
+};
