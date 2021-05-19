270edc895baa37b28a35c7738993d67d3d79f922
Muhammad Umer <myemailum14@gmail.com>
2021-05-19 01:02:12 -0400

    Create number-of-steps-to-reduce-a-number-to-zero.js

---
+++ leetcode/number-of-steps-to-reduce-a-number-to-zero.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var numberOfSteps = function(num) {
+    let steps = 0;
+    
+    while (num) {
+        if (num%2 === 0) {
+            num = num/2;
+            steps++
+        } else {
+            num--;
+            steps++;
+        }
+    }
+    return steps;
+};
