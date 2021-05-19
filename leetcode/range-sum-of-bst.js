7d1b3caaf743e6598ec79bf19542a13be9a3829a
Muhammad Umer <myemailum14@gmail.com>
2021-05-19 00:29:07 -0400

    Create range-sum-of-bst.js

---
+++ leetcode/range-sum-of-bst.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} low
+ * @param {number} high
+ * @return {number}
+ */
+var rangeSumBST = function(root, low, high) {
+    let sum = 0;
+    parseBST(root);
+    return sum;
+    
+    function parseBST(node) {
+        if (!node) return;
+        if (node.val >= low && node.val <= high) sum += node.val;
+        if (node.val > low) parseBST(node.left);
+        if (node.val < high) parseBST(node.right);
+
+    }
+};
